95567bc20b1137574eca762e7a73260a251d9df0
Richard Parayno <broodheart@gmail.com>
2018-09-08 18:50:25 +0800

    added logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,22 @@ import React from 'react'
 import TeaserContainer from '../components/TeaserContainer';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
+import logo from '../images/logo-white-colored.png';
+
 const TeaserPage = () => (
   <TeaserContainer>
     <Grid fluid style={{color: "#EFEFEF"}}>
       <Row center="xs">
         <Col xs={12} md={12} style={{marginTop: 150}}>
-          <h1 style={{fontFamily: "Inovato", fontSize: 80}}>Junior Form Function & Class 2019</h1>
+          <img className="logo" src={logo} alt="Junior Form Function & Class 2019 Logo"/>
         </Col>
         <Col xs={12} md={12} style={{marginTop: 50, marginBottom: 50}}>
           <p>January 26 - 27, 2019 / De La Salle University - Manila</p>
         </Col>
         <Col xs={12} md={7} style={{marginBottom: 150}}>
           <h2 style={{fontFamily: "Inovato", fontSize: 48}}>The premier student web design conference is coming soon.</h2>
-          <p>Registration opens September 2018. </p>
+          <p>Blind Bird Registration is now open.</p>
+          <a href="https://www.facebook.com/JuniorFFCPh/" style={{color: "#EFEFEF"}}>Get your tickets on Eventbrite.</a> <br/>
           <a href="https://www.facebook.com/JuniorFFCPh/" style={{color: "#EFEFEF"}}>Like our FB page for further announcements.</a>
         </Col>
       </Row>
